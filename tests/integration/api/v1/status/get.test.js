fea304ab037eb11dec5010e577ab0d9536f786fa
Lucas Víctor <168949817+LucasAzevedo0@users.noreply.github.com>
2025-12-30 20:30:37 +0000

    add integration tests to health metrics `/status`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,15 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const reponseBody = await response.json();
+  expect(reponseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(reponseBody.updated_at).toISOString();
+  expect(reponseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const dependencies = reponseBody.dependencies;
+  expect(dependencies.database.version).toEqual("16.0");
+  expect(dependencies.database.max_connections).toBe(100);
+  expect(dependencies.database.opened_connections).toBe(1);
 });
